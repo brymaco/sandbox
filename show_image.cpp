19959ca276fd735988ec85db68d7089db97c88db
brymaco <cbryman@gmail.com>
2018-06-07 22:50:17 -0700

    Initial project. Create .gitignore. Update CMakeLists.

--- show_image.cpp
+++ show_image.cpp
@@ -1,6 +1,23 @@
+#include <opencv2/opencv.hpp>
 #include <iostream>
 
-int main() {
-    std::cout << "Hello" << std::endl;
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        std::cout << "Provide a path to an image.\n";
+        return -1;
+    }
+
+    cv::Mat image;
+    image = cv::imread(argv[1], 1);
+    if (!image.data) {
+        std::cout << "No data.\n";
+        return -1;
+    }
+
+    cv::namedWindow("Show Image", cv::WINDOW_AUTOSIZE);
+    cv::imshow("Show Image", image);
+
+    cv::waitKey(0);
+
     return 0;
 }
